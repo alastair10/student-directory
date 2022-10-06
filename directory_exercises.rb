22ebf460396319715d3b2ee84b7ff46d449de473
Alastair <alastair.chau@gmail.com>
2022-10-06 11:19:55 +0100

    refactored by adding shorter methods

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -16,8 +16,6 @@ students = [
 ]
 =end 
 
-@students = [] # an empty array accessible to all methods
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -28,43 +26,13 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit # causes program to terminate
-    else
-      puts "I don't know what you meant, try again"
-  end
+  # return the array of students
+  students
 end
 
 def print_header
@@ -72,15 +40,24 @@ def print_header
   puts "-------------" 
 end
 
-def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print(students)
+  #students.each_with_index do |student, index|
+    #puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  #end
+  i = students.length 
+  while i > 0
+    puts students[:name][i]
+    i -= 1
   end
 end
 
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
 #nothing happents until we call the methods
-interactive_menu
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
